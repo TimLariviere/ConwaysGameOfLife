cf4538cb8a01dc237eb0ddfa555c84fc8ca7e97a
Timothé LARIVIERE <timothe.lariviere@outlook.com>
2018-01-08 19:06:02 +0100

    Array2D

--- FSharp/FSharp/Game.fs
+++ FSharp/FSharp/Game.fs
@@ -5,31 +5,21 @@ module Game =
     
     let mapWidth = 50
     let mapHeight = 50
-    let rulesBecomeAlive = [ 0; 3 ]
-    let rulesStayAlive = [ 2; 3; 5 ]
-
-    let private convertToPos index =
-        let x = index % mapWidth
-        let y = index / mapHeight
-        (x, y)
-
-    let private convertToIndex (x, y) =
-        x * mapWidth + y
+    let rulesBecomeAlive = [| 0; 3 |]
+    let rulesStayAlive = [| 2; 3; 5 |]
 
     let private transformPos (x, y) xMod yMod =
         (x + xMod, y + yMod)
     
-    let private posToInt (map: CellState array) (x, y) =
+    let private posToInt (map: CellState[,]) (x, y) =
         if (x >= 0 && x < mapWidth && y >= 0 && y < mapHeight) then
-            let index = convertToIndex (x, y)
-            match map.[index] with
+            match map.[x, y] with
             | ALIVE -> 1
             | DEAD -> 0
         else 0
 
-    let private getLivingNeighbors map index =
-        let pos = convertToPos index
-        let transformPos = transformPos pos
+    let private getLivingNeighbors map x y =
+        let transformPos = transformPos (x, y)
         let posToInt = posToInt map
 
         let topLeft     = transformPos -1 -1 |> posToInt
@@ -43,21 +33,21 @@ module Game =
         
         topLeft + top + topRight + left + right + bottomLeft + bottom + bottomRight
         
-    let private checkRules rules neighborsCount =
-        if (List.contains neighborsCount rules) then ALIVE else DEAD
+    let private checkRules rules neighborsCount = 
+        if (Array.contains neighborsCount rules) then ALIVE else DEAD
         
     let private checkBecomeAliveRules = checkRules rulesBecomeAlive
     let private checkStayAliveRules = checkRules rulesStayAlive
         
-    let private getNewState map index currentState =
-        let neighborsCount = getLivingNeighbors map index
+    let private getNewState map x y currentState =
+        let neighborsCount = getLivingNeighbors map x y
         match currentState with
         | ALIVE -> checkStayAliveRules neighborsCount
         | DEAD -> checkBecomeAliveRules neighborsCount
     
     let createEmptyMap() =
-        Array.init (mapWidth * mapHeight) (fun i -> DEAD)
+        Array2D.init mapWidth mapHeight (fun x y -> DEAD)
         
     let computeNextStep map =
         let getNewStateForMap = getNewState map            
-        map |> Array.mapi getNewStateForMap
+        map |> Array2D.mapi getNewStateForMap
